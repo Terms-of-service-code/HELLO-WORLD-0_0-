b7df52717fb3f0cc55b5a5d529d96777736c35af
Terms-of-service-code <Craft08.j@gmail.com>
2024-07-10 16:01:16 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,1 +1,56 @@
 print("Hello world")
+/*
+Declare Constants and Variables:
+
+Create constants for:
+
+App name (String)
+Developer name (String)
+Initial release year (Int)
+Create variables for:
+
+Current version (Double)
+Number of downloads (Int)
+User rating (Double, out of 5.0)
+Assign Initial Values:
+
+Assign appropriate initial values of your choosing to these constants and variables
+Print the initial values with descriptive text.
+Simulate an update where:
+
+The version number is incremented by 0.1
+The number of downloads increases by 1000
+The user rating improves by 0.2
+Print the updated values with descriptive text.
+Challenge Tasks:
+
+Create a new feature for your app:
+Add a new variable for the description of the new feature (String).
+Assign it a value and print it.
+Collect Feedback:
+Declare a variable to store the number of positive reviews (Int) and negative reviews (Int).
+Assign initial values and print the total number of reviews.
+Calculate and Print the New Rating:
+Assume each positive review adds 0.1 to the user rating and each negative review subtracts 0.1.
+Update the user rating accordingly and print the new rating.
+
+*/
+
+
+
+
+let Jaysapp =  "Jays App"
+let Developername = "Jeremiah"
+let Initialreleaseyear =  2024
+
+var Currentversion : Double = 1.20
+var Numberofdownloads = 3000
+var Userrating = 5.0
+
+
+print("This app is:\(Jaysapp) ")
+print("The developer of the game:\(Developername) ")
+print("Release Year:\(Initialreleaseyear) ")
+print("Version:\( Currentversion) ")
+print("Downloads:\( Numberofdownloads) ")
+print("App Rating:\( Userrating) ")
